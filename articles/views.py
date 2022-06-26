53bc15d4239b592af6af10b8c36d2611f7f47550
Priyanka <you@example.com>
2022-06-26 15:58:05 +0530

    My blogger

---
+++ articles/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView, DetailView
+from . models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'detail.html'
